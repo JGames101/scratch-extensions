b8a5e98b252a14035f2b1a0c3981dad603413a6e
JGames101 <jbmadden@hotmail.ca>
2017-08-10 11:47:25 -0700

    Update debugging.js

--- other/debugging.js
+++ other/debugging.js
@@ -1,1 +1,12 @@
-temp
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			[' ', 'console.log %s', 'logger', 'hello world']
+		],
+		url : 'https://jgames101.github.io/scratch-extensions/'
+	};
+  
+	ext.logger = function(logthing) {
+		console.log(logthing)
+	};
