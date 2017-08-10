a9d36a85785d0635ffc6357200d48a87dfccabaf
JGames101 <jbmadden@hotmail.ca>
2017-08-10 12:10:59 -0700

    Update browser.js

--- other/browser.js
+++ other/browser.js
@@ -2,7 +2,8 @@ new (function() {
 	var ext = this;
 	var descriptor = {
 		blocks: [
-			[' ', 'open tab %s', 'window', 'https://jgames101.github.io']
+			[' ', 'open tab %s', 'window', 'https://jgames101.github.io'],
+			[' ', 'refresh page', 'refresh'],
 		],
 		url : 'https://jgames101.github.io/scratch-extensions/'
 	};
@@ -16,7 +17,10 @@ new (function() {
   
 	ext.window = function(url) {
 		window.open(url);
-	}
+	};
+	ext.refresh = function() {
+		location.reload();
+	};
 	
 	ScratchExtensions.register('JGames101 | Browser', descriptor, ext);
 })();
