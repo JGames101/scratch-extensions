7496e39b6076a34be549e4a3b897b04756113a3f
JGames101 <jbmadden@hotmail.ca>
2017-08-10 10:47:00 -0700

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,1 +1,32 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+	    return {status: 2, msg: 'Extensions Loaded'};
+    };
+
+    ext.jinstalled = function() {
+        if (installed !== "granted")
+         return true;
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+	          ['b', 'jpack installed?', 'jinstalled']
+        ],
+	url: 'https://jgames101.github.io/scratch-extensions/'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('JGames101', descriptor, ext);
+    install()
+})({});
+function install() {
+  ScratchExtensions.loadExternalJS('https://jgames101.github.io/scratch-extensions/notifications/notifications.js');
+  ScratchExtensions.loadExternalJS('https://jgames101.github.io/scratch-extensions/test2.js');
+}
