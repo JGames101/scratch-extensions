d5bb570dafc7480b9170e723fef08636785290d2
JGames101 <jbmadden@hotmail.ca>
2017-08-10 03:24:15 -0700

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -8,9 +8,15 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.notify = function() {
-        // Code that gets executed when the block is run
-    };
+    ext.notify = function(name, content, image) {
+         if (Notification.permission !== "granted")
+            Notification.requestPermission();
+         else {
+            var notification = new Notification(name, {
+            icon: image,
+            body: content,
+	        title: name
+    }};
 
     // Block and block menu descriptions
     var descriptor = {
