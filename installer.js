b697866785f0413440fac86d98e1fcfb706c8cea
JGames101 <jbmadden@hotmail.ca>
2017-08-16 19:35:21 -0700

    Update installer.js

--- installer.js
+++ installer.js
@@ -36,4 +36,9 @@ function install() {
   ScratchExtensions.loadExternalJS('https://jgames101.github.io/scratch-extensions/other/debugging.js');
   ScratchExtensions.loadExternalJS('https://jgames101.github.io/scratch-extensions/other/browser.js');
   ScratchExtensions.loadExternalJS('https://jgames101.github.io/scratch-extensions/httpRequests/scratchAPI.js');
+  var script = document.createElement('script');
+  script.src = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js';
+  script.type = 'text/javascript';
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(script);
 }
