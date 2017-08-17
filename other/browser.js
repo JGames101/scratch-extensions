0507a1668cc411c12cad730dd368f308120ab41e
JGames101 <jbmadden@hotmail.ca>
2017-08-16 19:19:38 -0700

    Update browser.js

--- other/browser.js
+++ other/browser.js
@@ -5,7 +5,6 @@ new (function() {
 			[' ', 'open tab %s', 'window', 'https://jgames101.github.io'],
 			[' ', 'refresh page', 'refresh'],
 			[' ', 'history: pushstate %s url %s title %s', 'pushstate', 'home', '?page=home', 'Home'],
-			['h', 'popstate %s', 'popstate', 'home'],
 		],
 		url : 'https://jgames101.github.io/scratch-extensions/'
 	};
@@ -26,15 +25,6 @@ new (function() {
 	};
 	ext.pushstate = function(name, location, ttl) {
 		history.pushState(name, ttl, location);
-	};
-	ext.popstate = function(name) {
-		window.addEventListener('popstate', e => {
-			if (e.state.name === name) {
-				return true;
-			} else {
-				return false;
-			};
-		});
 	};
 	
 	ScratchExtensions.register('JGames101 | Browser', descriptor, ext);
