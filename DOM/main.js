81c73abbd123a1d9f2ac5582e1a280185ffb70ef
JGames101 <jbmadden@hotmail.ca>
2017-10-14 16:24:39 -0700

    Create main.js

---
+++ DOM/main.js
@@ -0,0 +1,22 @@
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			['b', 'Installed?', 'installed']
+		],
+		url : 'https://jgames101.github.io/scratch-extensions/'
+	};
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+  
+	ext.installed = function(logthing) {
+		return true;
+	}
+	
+	ScratchExtensions.register('HTML DOM | JGames101', descriptor, ext);
+})();
